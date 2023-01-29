124786e8c96c0fa0826dbf499820cad72956fe63
gabep <gabychoucroute16@gmail.com>
2023-01-29 23:01:06 +0100

    étape2 + test

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        String lang = System.getProperty("user.country");
+
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Bonjour, Entrez une chaîne de caractères : ");
@@ -11,13 +12,21 @@ public class Main {
 
         String inverse = new StringBuilder(chaine).reverse().toString();
 
-        if (chaine.equalsIgnoreCase(inverse) && chaine != "" ) {
+        if (lang == "FR" || chaine.equalsIgnoreCase(inverse) && chaine != "") {
             System.out.println("Bien joué ! La chaîne '"+chaine+"' est un palindrome.");
-        } else {
+        } else if(lang == "FR" || !chaine.equalsIgnoreCase(inverse)){
             System.out.println("Désolé, la chaîne '"+chaine+"' n'est pas un palindrome.");
+        } else if(lang == "EN" || chaine.equalsIgnoreCase(inverse) && chaine != "") {
+            System.out.println("Well done '"+chaine+"' is palindrome.");
+        } else if(lang == "EN" || !chaine.equalsIgnoreCase(inverse)) {
+            System.out.println("Sorry '"+chaine+"' is not palindrome.");
+        };
+        System.out.print(lang);
+        if(lang == "EN"){
+            System.out.print(" Good bye");
+        } else{
+            System.out.print(" Au revoir");
         }
-
-        System.out.print("Au revoir");
         scan.close();
     }
 }
