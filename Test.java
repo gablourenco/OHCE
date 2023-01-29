f425d721a42ad46a8a54c8ab6c170e37111c25be
gabep <gabychoucroute16@gmail.com>
2023-01-29 23:01:43 +0100

    étape 2 test

--- Test.java
+++ Test.java
@@ -19,4 +19,20 @@ public class MainTest {
         //ALORS le palindrome est renvoyé et bien dit est renvoyé
         System.out.println("'"+inverse+"' Bien dit !");
     }
+    public void testLang(String[] args){
+        // ETANT DONNER la langue de l'ordinateur de l'utilisateur
+        String lang = System.getProperty("user.country");
+
+        // QUAND on entre un palindrome
+        String chaine = "did";
+        String inverse = new StringBuilder(chaine).reverse().toString();
+        
+        //ALORS le palindrome est renvoyé et bien dit est renvoyé en fonction de la langue de la machine
+        String lang = System.getProperty("user.country");
+        if(lang = "FR" || chaine.equalsIgnoreCase(inverse) && chaine != "") {
+            System.out.println("Bien joué ! La chaîne '"+chaine+"' est un palindrome.");
+        } else {
+            System.out.println("Well done '"+chaine+"' est un palindrome.")
+        }
+    }
 }
